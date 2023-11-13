d59c30e0c7343e8605d59096c84ca0d33a6eac24
ThuyLe08072004 <133005861+ThuyLe08072004@users.noreply.github.com>
2023-11-13 15:05:48 +0700

    QuanLiDonHang

---
+++ app/src/main/java/com/example/thuyltph35992_quanlidonhang/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.thuyltph35992_quanlidonhang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
